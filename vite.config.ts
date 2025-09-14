658a584777cb183d15cb928fdc40c8a559f15932
adjebbar <45851643+adjebbar@users.noreply.github.com>
2025-09-14 22:10:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
+  base: "./",  // <-- important pour Vercel et GitHub Pages
   server: {
     host: "::",
     port: 8080,
@@ -15,3 +16,4 @@ export default defineConfig(() => ({
     },
   },
 }));
+
